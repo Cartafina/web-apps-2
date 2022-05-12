baffc3691fc020d1323cb899e1d190bd9572d5c8
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-05-12 02:55:59 +0000

    class

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,14 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
 
-  # TODO:
-  # def index
-  # end
+
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
   end
+
+  
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,15 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+def new
+  @post = Post.new
+end
+
+def create 
+  @post = Post.new
+  @post ["author"]
+  @post ["image"]
+  @post ["body"]
+  @post.save
+  redirect_to "/posts"
 end
